31d9dd233810747f502ddedd6d319333f26f7ecc
Paula Leitisstein Olsen <paulaolsen@paulas-macbook-air.local>
2019-05-19 12:01:44 +0200

    Initial Commit

---
+++ Paula-BellaTests/Paula_BellaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Paula_BellaTests.swift
+//  Paula-BellaTests
+//
+//  Created by Paula Leitisstein Olsen on 19/05/2019.
+//  Copyright © 2019 Paula Leitisstein Olsen. All rights reserved.
+//
+
+import XCTest
+@testable import Paula_Bella
+
+class Paula_BellaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
